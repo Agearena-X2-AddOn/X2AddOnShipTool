42b19c728f966a86af11523d39553d840ef429e4
Janworks <jan.wichelmann@hotmail.de>
2016-04-17 15:08:53 +0200

    Repository erstellt

---
+++ README.md
@@ -0,0 +1,3 @@
+Dieses Tool dient der einfachen Ankersetzung bei Schiffssegeln.
+
+Diese Software steht unter der MIT-Lizenz. Nähere Informationen sind der beigelegten LICENSE-Datei zu entnehmen.
---
+++ X2AddOnShipTool/ShipFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X2AddOnShipTool
+{
+	/// <summary>
+	/// Enthält Informationen über ein zu verankerndes Schiff. Entspricht im Prinzip einer Projektdatei.
+	/// </summary>
+	class ShipFile
+	{
+		#region Variablen
+
+
+		#endregion
+
+		#region Funktionen
+
+
+		#endregion
+	}
+}
